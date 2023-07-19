3ffa7b09cb1f46fec94e657faf3ba5f87f141738
JAYAPRASHANT <jayaprashanth.20eee@gmail.com>
2023-07-18 23:38:17 -0700

    prgm

--- Elementremoval.java
+++ Elementremoval.java
@@ -10,8 +10,20 @@ public class Elementremoval {
 		
 		for(int i=0;i<n;i++) {
 			a[i]=sc.nextInt();
+			int max=a[0];
+			
 			
 		}
+		for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] < a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+		}
+		
 		int cost=0;
 		for(int i=0;i<n;i++) {
 			cost+=(a[i]*(i+1));
@@ -21,3 +33,4 @@ public class Elementremoval {
 	}
 
 }
+
